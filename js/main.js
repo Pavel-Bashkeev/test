86f64851a1eee712e35915f68c55df0b8bfe3aab
Pavel <pavelbashkeev@gmail.com>
2020-09-18 16:46:52 +0300

    Это наш первый комит

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+// console.log(leftBordaerWidth);
+
+// var leftBordaerWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+
+let persone = {
+       name: "John",
+       age: 25,
+       isMaried: false
+};
+console.log(persone);
+console.log(persone.name);
+console.log(persone.last);
